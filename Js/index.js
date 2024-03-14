98087dd3b4f6f77b6a3f0b0fadd870965044a501
clement <clementadebola54@gmail.com>
2024-03-14 01:14:17 +0100

    new

--- Js/index.js
+++ Js/index.js
@@ -1,35 +1,35 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const showFormBtn = document.getElementById("showFormBtn");
-//     const formOverlay = document.getElementById("formOverlay");
-//     const closeFormBtn = document.getElementById("closeFormBtn");
-//     const successMessage = document.getElementById("successMessage");
+document.addEventListener("DOMContentLoaded", function() {
+    const showFormBtn = document.getElementById("showFormBtn");
+    const formOverlay = document.getElementById("formOverlay");
+    const closeFormBtn = document.getElementById("closeFormBtn");
+    const successMessage = document.getElementById("successMessage");
   
-//     showFormBtn.addEventListener("click", function() {
-//       formOverlay.style.display = "block";
-//     });
+    showFormBtn.addEventListener("click", function() {
+      formOverlay.style.display = "block";
+    });
   
-//     closeFormBtn.addEventListener("click", function() {
-//       formOverlay.style.display = "none";
-//     myForm.reset(); // Clear the form
-//     });
+    closeFormBtn.addEventListener("click", function() {
+      formOverlay.style.display = "none";
+    myForm.reset(); // Clear the form
+    });
   
-//     const myForm = document.getElementById("myForm");
-//     myForm.addEventListener("submit", function(e) {
-//       e.preventDefault(); // Prevent default form submission behavior
+    const myForm = document.getElementById("myForm");
+    myForm.addEventListener("submit", function(e) {
+      e.preventDefault(); // Prevent default form submission behavior
   
-//       // Here you would handle form submission to WhatsApp
-//       // Example: sendWhatsAppMessage();
+      // Here you would handle form submission to WhatsApp
+      // Example: sendWhatsAppMessage();
   
-//       // Show success message
-//       successMessage.style.display = "block";
+      // Show success message
+      successMessage.style.display = "block";
 
-//         // Clear the form
-//     myForm.reset();
+        // Clear the form
+    myForm.reset();
   
-//       // Hide success message after 5 seconds
-//       setTimeout(function() {
-//         successMessage.style.display = "none";
-//       }, 5000);
-//     });
-//   });
+      // Hide success message after 5 seconds
+      setTimeout(function() {
+        successMessage.style.display = "none";
+      }, 5000);
+    });
+  });
   
